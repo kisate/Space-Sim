7398511d60bc17ef49c405edac8c111868f29660
kisate <dhkas@mail.ru>
2017-11-08 01:22:53 +0300

    Управление камерой
    Сделал управление камерой(FOV, перемещение и поворот)

--- test/body.py
+++ test/body.py
@@ -1,19 +1,18 @@
 import numpy
 from panda3d.core import Geom
 class Body :
-	def __init__ (self, n,  pNode, m, p = numpy.array([0,0,0]), v = numpy.array([0,0,0]), av = numpy.array([0,0,0])) :
+	def __init__ (self, n, m, p = numpy.array([0,0,0]), v = numpy.array([0,0,0]), av = numpy.array([0,0,0])) :
 		self.mass = m
 		self.node = n
 		self.v = v
 		self.av = av
-		self.pNode = pNode
 		self.setPos(p)
 	
 	def setPos (self, p) :
 		self.pos = p
-		self.pNode.setPos(p[0],p[1],p[2])
+		self.node.setPos(p[0],p[1],p[2])
 	def getPos (self) :
-		return self.pNode.getPos()
+		return self.node.getPos()
 	
 	def setTrail(self, ls, gn) : 
 		self.lines = ls
--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ from panda3d.core import LVector3
 from direct.task import Task
 from direct.gui.OnscreenImage import OnscreenImage
 from panda3d.core import GeomVertexFormat, GeomVertexData, Geom, GeomNode, GeomLinestrips
-from panda3d.core import Material
+from panda3d.core import Material, WindowProperties, MouseButton
 from direct.gui.OnscreenText import OnscreenText
 from math import pi, sin, cos
 import numpy
@@ -23,6 +23,7 @@ tick = 0.003
 x = 35
 geoms = 600
 fov = 100
+scale = 1
 def getforce (o2, o1) :
 	k = o1.mass*o2.mass*g
 	d = k/numpy.linalg.norm(numpy.subtract(o2.pos, o1.pos))**3
@@ -33,7 +34,6 @@ class Test(ShowBase) :
 		log.info('Loading started')
 		ShowBase.__init__(self)
 		b = OnscreenImage(parent=render2d, image="textures/sky.jpg")
-		base.cam.node().getDisplayRegion(0).setSort(20)
 		self.gNode = GeomNode('gnode')
 		n = render.attachNewNode(self.gNode)
 		self.bodies = []
@@ -44,19 +44,23 @@ class Test(ShowBase) :
 		self.bodies[0].node.setMaterial(m)
 		NodePath(self.gNode).setMaterial(m)
 		self.taskMgr.doMethodLater(tick, self.physTask, 'PhysTask')
-		self.taskMgr.add(self.fovTask, 'fovTask')
-		base.cam.reparentTo(self.bodies[0].pNode)
-		base.cam.setPos(0,110000,0)
-		base.camLens.setFov(100)
-		base.cam.lookAt(0,0,0)
+		self.taskMgr.add(self.controllTask, 'ControllTask')
+		self.cameraSetup()
 		self.initText()
 		self.setUpKeys()
 		log.info('Loading done')
 	
+	def cameraSetup(self) :
+		self.cameraNode = render.attachNewNode('cameraNode')
+		camera.reparentTo(self.cameraNode)
+		base.camLens.setFov(100)
+		base.cam.node().getDisplayRegion(0).setSort(20)
+		self.cameraNode.reparentTo(self.bodies[0].node)
+		self.cameraNode.setCompass(render)
+	
 	def physTask(self, task) :
 		for o in self.bodies :
 			o.setPos(numpy.sum([o.pos, numpy.multiply(t, o.v)], axis = 0))
-			log.info('{0} {1}'.format(o.pNode.getPos(), o.node.getPos()))
 			o.lines.drawTo(o.getPos())
 			o.lines.create(o.gNode)
 			if (len(o.gNode.getGeoms()) > geoms) :
@@ -69,17 +73,27 @@ class Test(ShowBase) :
 					f = getforce(o2, o)
 					a = numpy.divide(f, o.mass)
 					o.v = numpy.sum([o.v, numpy.multiply(t, a)], axis = 0)
-		log.info(base.cam.getPos(base.render))
 		return Task.again
 		
-	def fovTask(self, task) :
+	def controllTask(self, task) :
 		global fov
 		if self.keys['fovup'] and fov < 360 :
-			fov+=0.5
+			fov+=(180-fov)/100
 			base.camLens.setFov(fov)
 		if self.keys['fovdown'] and fov > 0.5 :
-			fov-=0.5
+			fov-=(fov)/100
 			base.camLens.setFov(fov)
+		parent = self.cameraNode.getParent()
+		if self.keys['fwd'] == 1:
+			camera.setY(camera, 0.1)
+		if self.keys['bwd'] == 1:
+			camera.setY(camera, -0.1)
+			
+		if self.keys['lft'] == 1:
+			camera.setX(camera, -0.1)
+		if self.keys['rt'] == 1:
+			camera.setX(camera, 0.1)	
+		
 		return Task.cont
 	
 	def loadModels(self) : 
@@ -107,22 +121,70 @@ class Test(ShowBase) :
 		
 	def setUpKeys(self) :
 	
-		self.keys = {'fovup' : 0, 'fovdown' : 0}
+		self.keys = {'fovup' : 0, 'fovdown' : 0, 'fwd' : 0, 'bwd' : 0, 'lft' : 0, 'rt' : 0}
 		self.accept("escape", sys.exit)
 		self.accept("+", self.incSpeed)
 		self.accept("-", self.decSpeed)
 		self.accept("z", self.incScale)
 		self.accept("x", self.decScale)
 
-		self.accept('arrow_up', self.setKey, ['fovup', 1])
-		self.accept('arrow_up-up', self.setKey, ['fovup', 0])
-		self.accept('arrow_down', self.setKey, ['fovdown', 1])
-		self.accept('arrow_down-up', self.setKey, ['fovdown', 0])
-		self.accept
+		self.accept('arrow_up', self.setKey, ['fovdown', 1])
+		self.accept('arrow_up-up', self.setKey, ['fovdown', 0])
+		self.accept('arrow_down', self.setKey, ['fovup', 1])
+		self.accept('arrow_down-up', self.setKey, ['fovup', 0])
+		
+		self.accept('w', self.setKey, ['fwd', 1])
+		self.accept('w-up', self.setKey, ['fwd', 0])
+		self.accept('s', self.setKey, ['bwd', 1])
+		self.accept('s-up', self.setKey, ['bwd', 0])
+		self.accept('d', self.setKey, ['rt', 1])
+		self.accept('d-up', self.setKey, ['rt', 0])
+		self.accept('a', self.setKey, ['lft', 1])
+		self.accept('a-up', self.setKey, ['lft', 0])
+		
+		self.setUpMouse()
 	
 	def setKey(self, key, val) :
 		self.keys[key] = val
 	
+	def setUpMouse(self) :
+
+		self.disableMouse()
+
+		self.mouseMagnitude = 3
+
+		self.rotateX, self.rotateY = 0, 0
+
+		self.lastMouseX, self.lastMouseY = None, None
+
+		taskMgr.add(self.mouseTask, "Mouse Task")
+
+	def mouseTask (self, task):
+		mw = base.mouseWatcherNode
+		wp = WindowProperties()
+
+		hasMouse = mw.hasMouse() 
+		if hasMouse and mw.is_button_down(MouseButton.one()):
+			x, y = mw.getMouseX(), mw.getMouseY()
+			if self.lastMouseX is not None:
+				
+				dx, dy = x - self.lastMouseX, y - self.lastMouseY
+
+			else:
+				dx, dy = 0, 0
+
+			self.lastMouseX, self.lastMouseY = x, y
+			
+		else:
+			x, y, dx, dy, self.lastMouseX, self.lastMouseY = 0, 0, 0, 0, 0, 0
+
+		self.rotateX += dx * 10 * self.mouseMagnitude
+		self.rotateY += dy * 10 * self.mouseMagnitude
+
+		self.cameraNode.setH(self.rotateX)
+		self.cameraNode.setP(self.rotateY)
+		return Task.cont
+
 	def incSpeed(self) :
 		global t
 		t+= 20
@@ -145,22 +207,22 @@ class Test(ShowBase) :
 	def decScale(self) : 
 		factor = 3
 		global scale 
-		scale//= factor
-		self.scaleText.setText('scale : {0} [Z/X]'.format(scale))
-		for b in self.bodies :
-			b.node.setScale(b.node.getScale()[0]//factor)
+		if scale >= factor :
+			scale//= factor
+			for b in self.bodies :
+				b.node.setScale(b.node.getScale()[0]//factor)
+			self.scaleText.setText('scale : {0} [Z/X]'.format(scale))
 	
 	def addPlanet(self, name):
-		planet = loader.loadModel('models/sphere')
+		planet = loader.loadModel('models/planet_sphere')
 		planet.setTexture(loader.loadTexture('textures/' + name + '.jpg'))
 		r = values.values[name]['r']*scale
 		planet.setScale(r)
-		pn = render.attachNewNode(name)
-		planet.reparentTo(pn)
-		body = Body(planet, pn, values.values[name]['m'], numpy.array(values.values[name]['p']), numpy.array(values.values[name]['v']), numpy.array(values.values[name]['av']))
+		planet.reparentTo(render)
+		body = Body(planet, values.values[name]['m'], numpy.array(values.values[name]['p']), numpy.array(values.values[name]['v']), numpy.array(values.values[name]['av']))
 		lines = LineSegs()
 		lines.setThickness(1)
-		lines.moveTo(pn.getPos())
+		lines.moveTo(planet.getPos())
 		gn = lines.create()
 		m = Material()
 		m.setEmission((random.random() ,random.random() ,random.random() ,1))
@@ -169,6 +231,6 @@ class Test(ShowBase) :
 		body.setTrail(lines, gn)
 		self.bodies.append(body)
 		
-		
+
 test = Test()
 test.run()
